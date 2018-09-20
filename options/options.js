bfa3fa6c69baaf43071912880e7fe400a4be3e44
YUKI Hiroshi <shimoda@clear-code.com>
2018-09-20 11:47:35 +0900

    Add license headers.
    https://addons.mozilla.org/en-US/firefox/addon/lean-tab-limiter/ says that this addon is distributed under MPL2.0, but there were missing license headers.

--- options/options.js
+++ options/options.js
@@ -1,3 +1,9 @@
+/*
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
 async function restoreOptions () {
   const res = await browser.storage.local.get('tab-limit')
   document.querySelector("#tab-limit").value = res['tab-limit'] || browser.runtime.getManifest().DEFAULT_TAB_LIMIT
