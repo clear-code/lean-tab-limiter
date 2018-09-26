546cbcd4d62f8665cfd71b1d4a33a0c18ccd1040
YUKI Hiroshi <shimoda@clear-code.com>
2018-09-26 19:12:14 +0900

    Show notification message when tab creation is blocked

--- background.js
+++ background.js
@@ -7,6 +7,12 @@
 const DEFAULT_TAB_LIMIT = browser.runtime.getManifest().DEFAULT_TAB_LIMIT
 let limit = DEFAULT_TAB_LIMIT
 
+const DEFAULT_NOTIFY_BLOCKED = browser.runtime.getManifest().DEFAULT_NOTIFY_BLOCKED
+let notifyBlocked = DEFAULT_NOTIFY_BLOCKED
+
+const DEFAULT_NOTIFY_BLCOKED_DURATION = browser.runtime.getManifest().DEFAULT_NOTIFY_BLCOKED_DURATION
+let notifyBlockedDuration = DEFAULT_NOTIFY_BLCOKED_DURATION
+
 const updateLimit = async () => {
   const [localLimit, managedLimit] = await Promise.all([
     (async () => {
@@ -35,9 +41,23 @@ const updateLimit = async () => {
 browser.storage.onChanged.addListener(updateLimit)
 updateLimit()
 
+let clearNotification = null
+
 const reactToNewTab = async tab => {
   const tabs = await browser.tabs.query({})
-  if (tabs.length > limit) await browser.tabs.remove(tab.id)
+  if (tabs.length <= limit) return
+  browser.tabs.remove(tab.id)
+
+  if (!notifyBlocked) return
+  if (clearNotification) clearTimeout(clearNotification)
+  browser.notifications.create('notify-blocked', {
+    type:    'basic',
+    title:   browser.runtime.getManifest().name,
+    message: browser.i18n.getMessage('notify_blocked', [limit])
+  })
+  clearNotification = setTimeout(() => {
+    browser.notifications.clear('notify-blocked')
+  }, notifyBlockedDuration)
 }
 
 browser.tabs.onCreated.addListener(reactToNewTab)
