7483ed398c71a37b93c716555fa0baed7e632eb4
YUKI Hiroshi <shimoda@clear-code.com>
2018-09-26 19:35:43 +0900

    Add configuration UI for notification

--- options/options.js
+++ options/options.js
@@ -6,22 +6,28 @@
 
 async function initialize () {
   document.querySelector('#tab-limit-label').textContent = browser.i18n.getMessage('tab_limit_label')
+  document.querySelector('#notify-blocked-label').textContent = browser.i18n.getMessage('notify_blocked_label')
   document.querySelector('#save-button').textContent = browser.i18n.getMessage('save_button_label')
   document.querySelector('#default-button').textContent = browser.i18n.getMessage('default_button_label')
 
-  const res = await browser.storage.local.get('tab-limit')
-  document.querySelector("#tab-limit").value = res['tab-limit'] || browser.runtime.getManifest().DEFAULT_TAB_LIMIT
+  const res = await browser.storage.local.get({})
+  document.querySelector('#tab-limit').value = typeof res['tab-limit'] == 'number' ?
+    res['tab-limit'] : browser.runtime.getManifest().DEFAULT_TAB_LIMIT
+  document.querySelector('#notify-blocked').checked = typeof res['notify-blocked'] == 'boolean' ?
+    res['notify-blocked'] : browser.runtime.getManifest().DEFAULT_NOTIFY_BLOCKED
 }
 
 async function saveOptions () {
-  const newValue = document.querySelector("#tab-limit").value
+  const limit = document.querySelector("#tab-limit").value
+  const notifyBlocked = document.querySelector("#tab-limit").value
   await browser.storage.local.set({
-    'tab-limit': (!isNaN(newValue) && newValue > 1) ? newValue : browser.runtime.getManifest().DEFAULT_TAB_LIMIT
+    'tab-limit':      (!isNaN(limit) && limit > 1) ? limit : browser.runtime.getManifest().DEFAULT_TAB_LIMIT,
+    'notify-blocked': notifyBlocked
   })
 }
 
 async function backToDefault () {
-  await browser.storage.local.remove('tab-limit')
+  await browser.storage.local.remove(['tab-limit', 'notify-blocked'])
 }
 
 document.addEventListener('DOMContentLoaded', initialize)
